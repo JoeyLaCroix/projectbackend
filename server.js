9d0ebe83a6cfe5e683eb4aa52a13aa06b9d01765
joey LaCroix <JLACROIX@email.sc.edu>
2024-12-05 12:42:31 -0500

    mongodb

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ app.use(express.static("public"));
 app.use("/uploads", express.static("uploads"));
 app.use(express.json());
 app.use(cors());
+const mongoose = require("mongoose");
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -17,7 +18,28 @@ const storage = multer.diskStorage({
     },
   });
   
-  const upload = multer({ storage: storage });
+const upload = multer({ storage: storage });
+
+mongoose
+  .connect(
+    "mongodb+srv://JoeyLaCroix:if7mndfYTN3B1CQn@cluster0.oaf8k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch((error) => {
+    console.log("couldn't connect to mongodb", error);
+  });
+
+  const surferSchema = new mongoose.Schema({
+    name: String,
+    bio: String,
+    hometown: String,
+    surftype: String,
+    img_name: String,
+  });
+  
+  const Surfer = mongoose.model("Surfer", surferSchema);
 
 let surfers = [
     {
@@ -235,6 +257,17 @@ app.get("/api/surfers", (req,res)=>{
     res.json(surfers);
 });
 
+app.get("/api/surfers", async (req, res) => {
+    const surfers = await Surfer.find();
+    res.send(surfers);
+  });
+
+  app.get("/api/surfers/:id", async (req, res) => {
+    const surfer = await Surfer.findOne({ _id: id });
+    res.send(surfer);
+  });
+
+
 app.post("/api/surfers", upload.single("img"), (req, res) => {
     const result = validateSurfer(req.body);
   
@@ -259,7 +292,7 @@ app.post("/api/surfers", upload.single("img"), (req, res) => {
     res.status(200).send(surfer);
   });
 
-  app.put("/api/surfers/:id", upload.single("img"), (req, res) => {
+  app.put("/api/surfers/:id", upload.single("img"), async (req, res) => {
     let surfer = surfers.find((s) => s._id === parseInt(req.params.id));
   
     if (!surfer) res.status(400).send("Surfer with given id was not found");
@@ -270,6 +303,13 @@ app.post("/api/surfers", upload.single("img"), (req, res) => {
       res.status(400).send(result.error.details[0].message);
       return;
     }
+
+    let fieldsToUpdate = {
+        name: req.body.name,
+        bio: req.body.bio,
+        hometown: req.body.hometown,
+        surftype: req.body.surftype,
+      };
   
     surfer.name = req.body.name;
     surfer.hometown = req.body.hometown;
@@ -279,6 +319,14 @@ app.post("/api/surfers", upload.single("img"), (req, res) => {
     if (req.file) {
       surfer.main_image = "images/" + req.file.filename;
     }
+
+    const wentThrough = await Surfer.updateOne(
+        { _id: req.params.id },
+        fieldsToUpdate
+      );
+
+      const updatedSurfer = await Surfer.findOne({ _id: req.params.id });
+  res.send(updatedSurfer);
   
     res.send(surfer);
   });
